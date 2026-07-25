b6e28898b47aab4056542aab68b85a9b3ec03a1e
cw7430 <cw282@naver.com>
2026-07-25 17:05:37 +0900

    first commit

---
+++ src/main/java/com/izen/IzenApplication.java
@@ -0,0 +1,13 @@
+package com.izen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IzenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IzenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/izen/IzenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.izen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IzenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
