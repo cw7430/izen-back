36f5e516e7cc3d2b0c3c6d4e72e9fbd8b713c7f8
cw7430 <cw282@naver.com>
2026-07-31 16:52:11 +0900

    API KEY 설정

--- src/main/java/com/izen/common/config/web/OpenApiConfig.java
+++ src/main/java/com/izen/common/config/web/OpenApiConfig.java
@@ -2,7 +2,11 @@ package com.izen.common.config.web;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.media.StringSchema;
+import io.swagger.v3.oas.models.parameters.HeaderParameter;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.customizers.OpenApiCustomizer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -11,6 +15,13 @@ import org.springframework.http.HttpHeaders;
 @Configuration
 @Profile("!production")
 public class OpenApiConfig {
+
+    private final String secretApiKey;
+
+    public OpenApiConfig(@Value("${security.api-key}") String secretApiKey) {
+        this.secretApiKey = secretApiKey;
+    }
+
     @Bean
     public OpenAPI customOpenAPI() {
         SecurityScheme accessTokenScheme = new SecurityScheme()
@@ -33,5 +44,24 @@ public class OpenApiConfig {
                 .components(new Components()
                         .addSecuritySchemes("access-token", accessTokenScheme)
                         .addSecuritySchemes("refresh-token", refreshTokenScheme));
+    }
+
+    @Bean
+    public OpenApiCustomizer globalHeaderCustomizer() {
+        return openApi -> {
+            if (openApi.getPaths() != null) {
+                openApi.getPaths().values().forEach(pathItem ->
+                        pathItem.readOperations().forEach(operation -> {
+                            operation.addParametersItem(
+                                    new HeaderParameter()
+                                            .name("X-API-Key")
+                                            .description("자동 입력된 API Key")
+                                            .required(true)
+                                            .schema(new StringSchema()._default(secretApiKey))
+                            );
+                        })
+                );
+            }
+        };
     }
 }
